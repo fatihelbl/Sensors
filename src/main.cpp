3fbf8e0340495b46f1d38bf6f1b3cc14b8211dd8
fatihelbl <fatihelbl96@gmail.com>
2022-02-03 16:04:48 +0300

    main status ifadesi eklendi

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,23 @@
 
 NtcSensor iNtcSensor;
 TripleSensor iTripleSensor;
-SerialController iController(iNtcSensor,iTripleSensor); 
+SerialController iController(iNtcSensor, iTripleSensor);
 
-void setup() {
- Serial.begin(9600);
- Serial1.begin(9600,SERIAL_8N1);
+void setup()
+{
+  Serial.begin(9600);
+  Serial1.begin(9600, SERIAL_8N1);
 
- while (!Serial1 && !Serial)
- {
-   Serial.println(SERIAL_COMMUNATION_ERROR);
- }
- iTripleSensor.resetTimer();
- 
+  while (!Serial1 && !Serial)
+  {
+    Serial.println(SERIAL_COMMUNATION_ERROR);
+  }
+  iTripleSensor.resetTimer();
 }
 
-void loop() {
+void loop()
+{
   iController.serialRead();
   iTripleSensor.updateCo2();
+  //stash denendi
 }
