454d03b58776184c181efc242de28e56fe0e6196
fatihelbl <fatihelbl96@gmail.com>
2022-02-03 16:09:28 +0300

    git status ve pop kavramlari denendi.

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,25 @@
 
 NtcSensor iNtcSensor;
 TripleSensor iTripleSensor;
-SerialController iController(iNtcSensor,iTripleSensor); 
+SerialController iController(iNtcSensor, iTripleSensor);
 
-void setup() {
- Serial.begin(9600);
- Serial1.begin(9600,SERIAL_8N1);
+void setup()
+{
+  Serial.begin(9600);
+  Serial1.begin(9600, SERIAL_8N1);
 
- while (!Serial1 && !Serial)
- {
-   Serial.println(SERIAL_COMMUNATION_ERROR);
- }
- iTripleSensor.resetTimer();
- 
+  while (!Serial1 && !Serial)
+  {
+    Serial.println(SERIAL_COMMUNATION_ERROR);
+  }
+  iTripleSensor.resetTimer();
 }
 
-void loop() {
+void loop()
+{
   iController.serialRead();
   iTripleSensor.updateCo2();
+  /* if(stash){
+    stash deneniyor.
+  }   */
 }
